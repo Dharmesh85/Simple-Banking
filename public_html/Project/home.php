c629bf12978962f13169b1c6d2e1e76e85a56bd3
Dharmesh Patel <dharmeshpatel@dharmeshs-air.resource.campus.njit.edu>
2021-12-07 17:09:56 -0500

    Completed Transfer Between Their Account

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,14 +3,27 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 <head>
   <title>Home</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  
 </head>
-<h1>Bank of IT-202</h1>
+
+<body>
+    
+
+<div class="jumbotron text-center">
+  <h1> Bank of IT-202 </h1>
+</div>
+
+
 
 <?php
 
 
 if (is_logged_in(true)) {
-    echo "Welcome home, " . get_username();
+    echo "Welcome home, " . get_firstname();
     //comment this out if you don't want to see the session variables
     //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 }
--- public_html/Project/list_accounts.php
+++ public_html/Project/list_accounts.php
@@ -36,7 +36,7 @@ require_once(__DIR__ . "/../../lib/functions.php")
                     </div>
                     <div>
                         
-                        <a type="button" href="<?php echo get_url("transactions.php?id=" . $r["AccID"]); ?>">View Transaction History</a>
+                        <a type="button" href="<?php echo get_url("view_transactions.php?id=" . $r["AccID"]); ?>">View Transaction History</a>
                     </div>
                 </div>
             <?php endforeach; ?>
