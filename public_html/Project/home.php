924fb3ac43b877b77da10a92d809b3cd198eaf74
Dharmesh Patel <dharmeshpatel@Dharmeshs-MacBook-Air.local>
2021-12-19 22:51:03 -0500

    Completed Public and Private Account

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,5 +1,10 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+if (!is_logged_in()) {
+  //this will redirect to login and kill the rest of this script (prevent it from executing)
+  flash("You don't have permission to access this page");
+  die(header("Location: login.php"));
+}
 ?>
 <head>
   <title>Home</title>
@@ -7,26 +12,14 @@ require(__DIR__ . "/../../partials/nav.php");
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
 </head>
-
 <body>
-    
 
 <div class="jumbotron text-center">
-  <h1> Bank of IT-202 </h1>
+<h1 class="display-4">Welcome, <?php echo(get_firstname()); ?>!</h1>
 </div>
 
 
-
-<?php
-
-if (is_logged_in(true)) {
-    echo '<div style="font-size:2.25em;color:black"> Welcome home, </div>' . get_firstname();
-    //comment this out if you don't want to see the session variables
-    //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-}
-?>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
