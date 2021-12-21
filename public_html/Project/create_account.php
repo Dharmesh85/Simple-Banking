8b6cdf222b99eff8be0b3be8a419fff2de62583c
Dharmesh Patel <dharmeshpatel@Dharmeshs-MacBook-Air.local>
2021-12-20 23:35:16 -0500

    Adding changes

--- public_html/Project/create_account.php
+++ public_html/Project/create_account.php
@@ -46,7 +46,7 @@ if (isset($_POST["save"])) {
   $db = getDB();
   $check = $db->prepare('SELECT account_number FROM Accounts WHERE account_number = :q');
   do {
-    $account_number = rand(100000000000, 999999999999);
+    $account_number = get_random_str(12);
     $check->execute([':q' => $account_number]);
   } while ( $check->rowCount() > 0 );
 
